2cec748824b096e0805e5991c29836aac55d6a42
SerSmoothie <buster_hultgren@hotmail.com>
2020-09-01 07:11:06 +0200

    Grammar adding and getting production rules now work correctly

--- src/main/se/umu/cs/warn/buster/cyk/Grammar.java
+++ src/main/se/umu/cs/warn/buster/cyk/Grammar.java
@@ -21,6 +21,8 @@ public class Grammar {
         start = EOF;
         nonTerminals = null;
         terminals = null;
+        nonTerminalProductions = new HashMap<>();
+        terminalProductions = new HashMap<>();
     }
 
     public char[] produceRandom(char production, int length) {
@@ -116,9 +118,81 @@ public class Grammar {
     }
 
     public void setTerminals(char[] terminals) throws IllegalArgumentException {
-        if (start < 'a' || start > 'a')
-            throw new IllegalArgumentException("Error terminal symbol: " + start + "\nAll terminals must be a char" +
-                    " between lower case a-z");
+        for (char terminal : terminals) {
+            if (terminal < 'a' || terminal > 'z')
+                throw new IllegalArgumentException("Error terminal symbol: " + terminal + "\nAll terminals must be a " +
+                        "char between lower case a-z");
+        }
         this.terminals = terminals;
+    }
+
+    public void addProductionRule(char from, String production) {
+        if (production.length() != 2)
+            throw new IllegalArgumentException("Error from production rule: " + from + " -> " + production +
+                    "\nProduction does not follow CNF");
+
+        if (!charArrayContains(nonTerminals, from))
+            throw new IllegalArgumentException("Error from: " + from + "\nIs not a member of non terminal " +
+                    "productions: " + production);
+
+        for (int i = 0; i < production.length(); i++) {
+            if (!charArrayContains(nonTerminals, production.charAt(i)))
+                throw new IllegalArgumentException("Error from: " + production.charAt(i) + "\nProduction is not a " +
+                        "member of non terminal productions: " + nonTerminalProductions);
+        }
+
+        String[] newProductions;
+        if (nonTerminalProductions.containsKey(from)) {
+            String[] previousProductions = nonTerminalProductions.get(from);
+            newProductions = new String[previousProductions.length + 1];
+            System.arraycopy(previousProductions, 0, newProductions, 0, previousProductions.length);
+            newProductions[newProductions.length - 1] = production;
+        } else {
+            newProductions = new String[1];
+            newProductions[0] = production;
+        }
+        nonTerminalProductions.put(from, newProductions);
+    }
+
+    public void addProductionRule(char from, char production) {
+
+        if (nonTerminals == null)
+            throw new IllegalStateException("Error! Cannot add a production with no non terminals");
+
+        if (!charArrayContains(nonTerminals, from))
+            throw new IllegalArgumentException("Error from nonTerminal: " + from + "\nIs not a member of non terminal" +
+                    " productions: " + nonTerminals);
+
+        if (!charArrayContains(terminals, production))
+            throw new IllegalArgumentException("Error from to production: " + production + "\nProduction is not a " +
+                    "member of non terminal productions: " + nonTerminalProductions);
+
+        Character[] newProductions;
+        if (terminalProductions.containsKey(from)) {
+            Character[] previousProductions = terminalProductions.get(from);
+            newProductions = new Character[previousProductions.length + 1];
+            for (int i = 0; i < previousProductions.length; i++) {
+                newProductions[i] = previousProductions[i];
+            }
+            newProductions[newProductions.length - 1] = production;
+        } else {
+            newProductions = new Character[1];
+            newProductions[0] = production;
+        }
+        terminalProductions.put(from, newProductions);
+    }
+
+    public String[] getNonTerminalProductions(char from) {
+        if (from < 'A' || from > 'Z')
+            throw new IllegalArgumentException("Error with input: " + from + "\nNon terminal symbols must bee " +
+                    "between capital A-Z");
+        return nonTerminalProductions.get(from);
+    }
+
+    public Character[] getTerminalProductions(char from) {
+        if (from < 'A' || from > 'Z')
+            throw new IllegalArgumentException("Error with input: " + from + "\nNon terminal symbols must bee " +
+                    "between capital A-Z");
+        return terminalProductions.get(from);
     }
 }
--- src/test/se/umu/cs/warn/buster/cyk/GrammarTest.java
+++ src/test/se/umu/cs/warn/buster/cyk/GrammarTest.java
@@ -2,68 +2,135 @@ package se.umu.cs.warn.buster.cyk;
 
 import org.junit.Assert;
 import org.junit.Test;
-import se.umu.cs.warn.buster.cyk.Grammar;
 
 import static org.junit.Assert.*;
 
-class GrammarTest {
+public class GrammarTest {
 
     private Grammar grammar;
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-        if (grammar == null)
-            grammar = new Grammar();
+    private void grammarSetUp() {
+        grammar = new Grammar();
+        grammar.setStart('S');
+        grammar.setNonTerminals("SAB".toCharArray());
+        grammar.setTerminals("ab".toCharArray());
     }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-        grammar = null;
+    private void setGrammarWithProductions() {
+        grammarSetUp();
+        grammar.addProductionRule('S', "AB");
+        grammar.addProductionRule('A', "A");
+        grammar.addProductionRule('A', 'a');
+        grammar.addProductionRule('B', "BA");
+        grammar.addProductionRule('B', "BB");
+        grammar.addProductionRule('B', 'b');
     }
 
     @Test
     public void testSetStart() {
+        grammar = new Grammar();
         grammar.setStart('S');
         Assert.assertEquals('S', grammar.getStart());
     }
 
     @Test
     public void testSetNonTerminals() {
+        grammar = new Grammar();
         grammar.setNonTerminals("SAB".toCharArray());
         Assert.assertArrayEquals("SAB".toCharArray(), grammar.getNonTerminals());
     }
 
     @Test
     public void testSetTerminals() {
+        grammar = new Grammar();
         grammar.setTerminals("ab".toCharArray());
         Assert.assertArrayEquals("ab".toCharArray(), grammar.getTerminals());
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void testSetFaultyStart()  throws IllegalArgumentException {
+        grammar = new Grammar();
         grammar.setStart('s');
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void testSetFaultyNonTerminals()  throws IllegalArgumentException {
+        grammar = new Grammar();
         grammar.setNonTerminals("sab".toCharArray());
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void testSetFaultyTerminals()  throws IllegalArgumentException {
+        grammar = new Grammar();
         grammar.setTerminals("AB".toCharArray());
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void testSetStartWhichDoesNotBelongToNonTerminals()  throws IllegalArgumentException {
+        grammar = new Grammar();
         grammar.setTerminals("AB".toCharArray());
         grammar.setStart('S');
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void testNonTerminalsWhichDoesNotContainStart()  throws IllegalArgumentException {
+        grammar = new Grammar();
         grammar.setStart('S');
         grammar.setTerminals("AB".toCharArray());
+    }
+
+    @Test
+    public void testAddNonTerminalProduction() {
+        grammarSetUp();
+        grammar.addProductionRule('S', "AB");
+        assertEquals("AB", grammar.getNonTerminalProductions('S')[0]);
+    }
+
+    @Test
+    public void testAddTerminalProduction() {
+        grammarSetUp();
+        grammar.addProductionRule('A', 'a');
+        if (grammar == null)
+            System.out.println("WHAT");
+        char production = grammar.getTerminalProductions('A')[0];
+        assertEquals('a', production);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testAddNonTerminalProductionWithFaultNonExistingNonFromTerminal() {
+        grammarSetUp();
+        grammar.addProductionRule('C', "AB");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testAddNonTerminalProductionWithFaultNonExistingToNonTerminal() {
+        grammarSetUp();
+        grammar.addProductionRule('A', "AC");
+        System.out.println("Here lad");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testAddNonTerminalProductionWithFaultNotInCNF1() {
+        grammarSetUp();
+        grammar.addProductionRule('A', "A");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testAddNonTerminalProductionWithFaultNotInCNF2() {
+        grammarSetUp();
+        grammar.addProductionRule('A', "ABA");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testAddTerminalProductionWithFaultNonExistingNonTerminal() {
+        grammarSetUp();
+        grammar.addProductionRule('C', 'a');
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testAddNonTerminalProductionWithFaultNonExistingTerminal() {
+        grammarSetUp();
+        grammar.addProductionRule('A', 'c');
     }
 
     @Test
