e97485c996b69304135d46196cb0059713e68f65
SerSmoothie <buster_hultgren@hotmail.com>
2020-09-14 19:56:32 +0200

    Finnished implementation of Bottom Up Algorithm.

--- src/main/se/umu/cs/warn/buster/cyk/bottomup/CYKBottomUp.java
+++ src/main/se/umu/cs/warn/buster/cyk/bottomup/CYKBottomUp.java
@@ -3,6 +3,8 @@ package se.umu.cs.warn.buster.cyk.bottomup;
 import se.umu.cs.warn.buster.cyk.CYKStrategy;
 import se.umu.cs.warn.buster.cyk.Grammar;
 
+import java.util.Arrays;
+
 /**
  * Author: Buster Hultgren Wärn - busterw@cs.umu.se
  *
@@ -11,20 +13,107 @@ import se.umu.cs.warn.buster.cyk.Grammar;
 public class CYKBottomUp implements CYKStrategy {
 
     private Grammar grammar;
-    private String[] mem;
 
     @Override
     public boolean parse(char[] string) {
 
-        // compute upper bound matrix triangle size
-        int size = (int) Math.ceil((Math.pow(string.length, 2) / 2 + 0.5 * string.length));
-        mem = new String[size];
+        String[][] mem = new String[string.length][];
+
+        for (int i = 0; i < string.length; i++) {
+            mem[i] = new String[string.length - i];
+            Arrays.fill(mem[i], "");
+        }
+
+        for (int i = 0; i < string.length; i++) {
+
+            if (i == 0) {
+                for (int j = 0; j < string.length; j++)
+                    mem[i][j] = grammar.findCartesianProductFromProduction(String.valueOf(string[j]));
+            } else {
+                for (int j = 0; (j + i) < string.length; j++) {
+                    for (int k = j; k < (j + i); k++) {
+                        String leftSideProduction = mem[(k - j)][j];
+                        String rightSideProduction = mem[(j + i - k - 1)][(k + 1)];
+                        for (int n = 0; n < leftSideProduction.length(); n++)
+                            for (int m = 0; m < rightSideProduction.length(); m++)
+                                mem[i][j] = grammar.findCartesianProductFromProduction("" +
+                                        leftSideProduction.charAt(n) + rightSideProduction.charAt((m)));
+                    }
+                }
+            }
+        }
+
+        if (mem[string.length-1][0].contains(String.valueOf(grammar.getStart())))
+            return true;
+
         return false;
     }
 
-    private boolean parseStringRecursively(char[] string) {
+    /*
+
+
+    @Override
+    public boolean parse(char[] string) {
+
+        System.out.println("String length: " + string.length + "\r\n");
+        // compute upper bound matrix triangle size
+        int size = (int) Math.ceil((Math.pow(string.length, 2) / 2 + 0.5 * string.length));
+        mem = new String[string.length][];
+
+        for (int i = 0; i < string.length; i++) {
+            mem[i] = new String[string.length - i];
+            Arrays.fill(mem[i], "");
+        }
+
+        for (int i = 0; i < string.length; i++) {
+
+            if (i == 0) {
+                for (int j = 0; j < string.length; j++)
+                    mem[i][j] = grammar.findCartesianProductFromProduction(String.valueOf(string[j]));
+            } else {
+
+                System.out.println("Check " + (i + 1) + " length: " + (i + 1));
+                for (int j = 0; (j + i) < string.length; j++) {
+                    System.out.print("Checking: " + j + "-" + (j + i));
+                    for (int k = j; k < (j + i); k++) {
+                        //System.out.print("[" + k + " " + (k + 1) + "] ");
+                        System.out.print(" (");
+                        for (int q = j; q < k + 1; q++)
+                            System.out.print(q);
+                        System.out.print(" ");
+                        for (int q = k + 1; q <= (j + i); q++)
+                            System.out.print(q);
+                        System.out.print(")");
+                        //System.out.print("(" + (k + 1 - j) + "," + (j + i - k) + "-");
+                        //System.out.print(mem[k-j][j] + "" + mem[k-j][0] + ")");
+                        System.out.print("mem["+(k-j)+"]["+j+"] mem["+(j+i-k-1)+"]["+(k+1)+"]");
+                        //System.out.print(mem[(k-j)][j] + mem[(j+i-k-1)][(k+1)]);
+                        //System.out.print(grammar.findCartesianProductFromProduction(mem[(k-j)][j] + mem[(j+i-k-1)][(k+1)]));
+                        String leftSideProduction = mem[(k-j)][j];
+                        String rightSideProduction = mem[(j+i-k-1)][(k+1)];
+                        for (int n = 0; n < leftSideProduction.length(); n++)
+                            for (int m = 0; m < rightSideProduction.length(); m++)
+                                mem[i][j] = grammar.findCartesianProductFromProduction("" + leftSideProduction.charAt(n) + rightSideProduction.charAt((m)));
+                        //System.out.print(" - ijk: " + i+j+k);
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+            }
+        }
+
+        for (int i = 0; i < mem.length; i++) {
+            for (int j = 0; j < mem[i].length; j++)
+                System.out.print("[" + i + "][" + j + "] = " + mem[i][j] + "\t");
+            System.out.println();
+        }
+
+        if (mem[string.length-1][0].contains(String.valueOf(grammar.getStart())))
+            return true;
+
         return false;
     }
+     */
 
     @Override
     public String getName() {
@@ -36,7 +125,5 @@ public class CYKBottomUp implements CYKStrategy {
         this.grammar = grammar;
     }
 
-    private int computeUpperBoundMatrixSize(int n) {
-        return 2;
-    }
+
 }
---
+++ src/test/se/umu/cs/warn/buster/cyk/bottomup/CYKBottomUpTest.java
@@ -0,0 +1,183 @@
+package se.umu.cs.warn.buster.cyk.bottomup;
+
+import org.junit.Test;
+import se.umu.cs.warn.buster.cyk.CYKStrategy;
+import se.umu.cs.warn.buster.cyk.Grammar;
+
+import static org.junit.Assert.*;
+
+public class CYKBottomUpTest {
+
+
+    private CYKStrategy setStrategyWithSABGrammar() {
+        Grammar grammar = new Grammar();
+        grammar.setStart('S');
+        grammar.setNonTerminals("SAB".toCharArray());
+        grammar.setTerminals("ab".toCharArray());
+        grammar.addProductionRule('S', "AB");
+        grammar.addProductionRule('A', "BA");
+        grammar.addProductionRule('A', 'a');
+        grammar.addProductionRule('B', "AB");
+        grammar.addProductionRule('B', 'b');
+
+        CYKStrategy strategy = new CYKBottomUp();
+        strategy.setGrammar(grammar);
+        return strategy;
+    }
+
+    private CYKStrategy setStrategyWithParenthesisGrammar() {
+        Grammar grammar = new Grammar();
+        grammar.setStart('S');
+        grammar.setNonTerminals("SABLR".toCharArray());
+        grammar.setTerminals("b()".toCharArray());
+        grammar.addProductionRule('S', "SS");
+        grammar.addProductionRule('S', "LA");
+        grammar.addProductionRule('S', "LR");
+        grammar.addProductionRule('S', 'b');
+        grammar.addProductionRule('A', "SR");
+        grammar.addProductionRule('L', '(');
+        grammar.addProductionRule('R', ')');
+
+        CYKStrategy strategy = new CYKBottomUp();
+        strategy.setGrammar(grammar);
+        return strategy;
+    }
+
+    @Test
+    public void testCorrectAB0() {
+        CYKStrategy strategy = setStrategyWithSABGrammar();
+        assertEquals(true, strategy.parse("ab".toCharArray()));
+    }
+
+    @Test
+    public void testCorrectAB1() {
+        CYKStrategy strategy = setStrategyWithSABGrammar();
+        assertEquals(true, strategy.parse("abab".toCharArray()));
+    }
+
+    @Test
+    public void testCorrectAB2() {
+        CYKStrategy strategy = setStrategyWithSABGrammar();
+        assertEquals(true, strategy.parse("ababab".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyAB0() {
+        CYKStrategy strategy = setStrategyWithSABGrammar();
+        assertEquals(false, strategy.parse("a".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyAB1() {
+        CYKStrategy strategy = setStrategyWithSABGrammar();
+        assertEquals(false, strategy.parse("b".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyAB2() {
+        CYKStrategy strategy = setStrategyWithSABGrammar();
+        assertEquals(false, strategy.parse("c".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyAB3() {
+        CYKStrategy strategy = setStrategyWithSABGrammar();
+        assertEquals(false, strategy.parse("ba".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyAB4() {
+        CYKStrategy strategy = setStrategyWithSABGrammar();
+        assertEquals(false, strategy.parse("aba".toCharArray()));
+    }
+
+    @Test
+    public void testCorrectParenthesis0() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(true, strategy.parse("()".toCharArray()));
+    }
+
+    @Test
+    public void testCorrectParenthesis1() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(true, strategy.parse("(b)".toCharArray()));
+    }
+
+    @Test
+    public void testCorrectParenthesis2() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(true, strategy.parse("(bb)".toCharArray()));
+    }
+
+    @Test
+    public void testCorrectParenthesis3() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(true, strategy.parse("(bbbbbbbbbb)".toCharArray()));
+    }
+
+    @Test
+    public void testCorrectParenthesis5() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(true, strategy.parse("(b)()".toCharArray()));
+    }
+
+    @Test
+    public void testCorrectParenthesis5_2() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(true, strategy.parse("(bbb)(b)".toCharArray()));
+    }
+
+    @Test
+    public void testCorrectParenthesis6() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(true, strategy.parse("(()b())".toCharArray()));
+    }
+
+    @Test
+    public void testCorrectParenthesis7() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(true, strategy.parse("(bb(b)b(b()b)b)()".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyParenthesis0() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(false, strategy.parse(")(".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyParenthesis1() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(false, strategy.parse("(bbbbb(".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyParenthesis2() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(false, strategy.parse("(bbbb)b)".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyParenthesis3() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(false, strategy.parse(")bb)".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyParenthesis4() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(false, strategy.parse("(b(bbbbb)".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyParenthesis5() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(false, strategy.parse("b(bb(bbb)".toCharArray()));
+    }
+
+    @Test
+    public void testFaultyParenthesis6() {
+        CYKStrategy strategy = setStrategyWithParenthesisGrammar();
+        assertEquals(false, strategy.parse("b(bbabbb)".toCharArray()));
+    }
+}
